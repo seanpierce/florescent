7ded550cc6ff53113efee096aa97a71d21b4b846
Sean Pierce <sumler.sean@gmail.com>
2020-07-12 11:33:20 -0700

    Add API call to collect images and styles

--- app/components/Gallery.vue
+++ app/components/Gallery.vue
@@ -1,25 +1,38 @@
 <template>
     <div>
         <div>Gallery</div>
-        <v-lazy-image src="https://cdn-images-1.medium.com/max/1600/1*xjGrvQSXvj72W4zD6IWzfg.jpeg"
-            src-placeholder="https://cdn-images-1.medium.com/max/80/1*xjGrvQSXvj72W4zD6IWzfg.jpeg" />
-        <v-lazy-image src="https://cdn-images-1.medium.com/max/1600/1*xjGrvQSXvj72W4zD6IWzfg.jpeg"
-            src-placeholder="https://cdn-images-1.medium.com/max/80/1*xjGrvQSXvj72W4zD6IWzfg.jpeg" />
-        <v-lazy-image src="https://cdn-images-1.medium.com/max/1600/1*xjGrvQSXvj72W4zD6IWzfg.jpeg"
-            src-placeholder="https://cdn-images-1.medium.com/max/80/1*xjGrvQSXvj72W4zD6IWzfg.jpeg" />
-        <v-lazy-image src="https://cdn-images-1.medium.com/max/1600/1*xjGrvQSXvj72W4zD6IWzfg.jpeg"
-            src-placeholder="https://cdn-images-1.medium.com/max/80/1*xjGrvQSXvj72W4zD6IWzfg.jpeg" />
-        <v-lazy-image src="https://cdn-images-1.medium.com/max/1600/1*xjGrvQSXvj72W4zD6IWzfg.jpeg"
-            src-placeholder="https://cdn-images-1.medium.com/max/80/1*xjGrvQSXvj72W4zD6IWzfg.jpeg" />
+        <div id="gallery-images">
+            <div class="gallery-image" v-for="image in images" :key="image.id">
+                <v-lazy-image :src="'media/' + image.image"
+                    src-placeholder="https://cdn-images-1.medium.com/max/80/1*xjGrvQSXvj72W4zD6IWzfg.jpeg" />
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+import axios from 'axios';
 import VLazyImage from "v-lazy-image";
 
 export default {
     components: {
         VLazyImage
+    },
+    data() {
+        return {
+            images: null
+        }
+    },
+    methods: {
+        getImages() {
+            axios.get('/api/gallery/images')
+                .then(response => {
+                    this.images = response.data;
+                })
+        }
+    },
+    mounted() {
+        this.getImages();
     }
 }
 </script>
