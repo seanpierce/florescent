c62c2b337cf148006b7dc72d5c1e96b547758e90
Sean Pierce <sumler.sean@gmail.com>
2020-08-12 14:59:35 -0700

    Add vuex and router

---
+++ app/src/store/index.js
@@ -0,0 +1,27 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+// import axios from 'axios'
+// import settings from '@/settings.js'
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+    galleryImages: []
+  },
+  mutations: {
+    // INSERT_GALLERY_IMAGES: (store, images) => {
+    //   store = store.galleryImages.concat(images);
+    // }
+  },
+  actions: {
+    // getGalleryImages: context => {
+    //   axios.get(settings.API_BASE_URL + '/api/gallery/images')
+    //     .then(res => {
+    //       context.commit('INSERT_GALLERY_IMAGES', res.data);
+    //     })
+    // }
+  },
+  modules: {
+  }
+})
